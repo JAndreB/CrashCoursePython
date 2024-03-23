56a1088820b9c01601b6df4427929f0102ae7cfb
JAndreB <andrebyfield@live.co.uk>
2024-03-23 12:57:04 +0000

    Update

---
+++ Ch4/foods.py
@@ -0,0 +1,11 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('cucumber')
+
+print("My favourite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
