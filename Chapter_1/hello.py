9c37b9245fd27fb004ba9fd869a6157fb6660e46
JAndreB <andrebyfield@live.co.uk>
2024-09-07 18:32:11 +0100

    Updates

---
+++ Chapter_1/hello.py
@@ -0,0 +1,11 @@
+# this program says hello and asks for your name
+
+print('Hello World!')
+print('Whats your name?') # Ask for my name
+myName = input()
+print("It's good to meet you," + myName + ".")
+print("THe length of your name is:")
+print(len(myName))
+print("What is your age?")   # ASk for my age
+myAge = input()
+print("You wil be " + str(int(myAge) + 1) + " in a year!")
---
+++ Chapter_1/printRandom.py
@@ -0,0 +1,3 @@
+import random
+for i in range(99):
+    print(random.randint(1, 10))
