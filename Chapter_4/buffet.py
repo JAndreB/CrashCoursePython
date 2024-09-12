6c0d654a451a9ac4ff586eb072d71340c53a0d1c
JAndreB <andrebyfield@live.co.uk>
2024-09-12 21:05:17 +0100

    Update

---
+++ Chapter_4/buffet.py
@@ -0,0 +1,12 @@
+buffet  = ('lager', 'monkey brains', 'fish', "rabbit's foot", 'leeks')
+print("\nMenu: ")
+for i in buffet:
+    print(i)
+
+#No Workey
+#buffet[0] = "mildew"
+
+buffet = ('lager', 'monkey brains', 'fish', "cow's foot", 'ribeye')
+print("\nMenu: ")
+for i in buffet:
+    print(i)
